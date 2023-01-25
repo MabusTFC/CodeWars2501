ffb3ad7bc729394a686c8e1cae61363a31d60e02
MABUS <vanechka.smirnov.04@inbox.ru>
2023-01-25 13:57:32 +0500

    Добавьте файлы проекта.

---
+++ CodeWars2501/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9), которая возвращает строку этих чисел в виде номера телефона.*/
+
+using Microsoft.VisualBasic;
+using System;
+
+namespace word2501
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            string[] nomer = new string[10];
+            Console.WriteLine("Введите цифры подряд:");
+            for (int i = 0; i < nomer.Length; i++)
+            {
+                nomer[i] =Console.ReadLine();
+            }
+            string str = String.Join("", nomer);
+
+            Console.WriteLine("Ваш номер телефона:",str);
+
+        }
+
+        
+
+    }
+}
